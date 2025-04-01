4d16274f8083a574277a6ed7db918cb73ee2d6fb
soham0w0sarkar <sohamsarkar59@gmail.com>
2025-04-01 11:41:38 +0530

    fix

--- Business#1/corporate_b2b/accounts/urls.py
+++ Business#1/corporate_b2b/accounts/urls.py
@@ -1,19 +1,24 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from dashboard.views import dashboard_view
 from . import views
+
+app_name = "accounts"
 
 urlpatterns = [
     # Authentication
-    path('login/', auth_views.LoginView.as_view(
-        template_name='accounts/login.html',
-        extra_context={'site_name': 'Ambro Bites'}
-    ), name='login'),
-    
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="accounts/login.html",
+            extra_context={"site_name": "Ambro Bites"},
+        ),
+        name="login",
+    ),
+    path("", dashboard_view, name="home"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     # Registration
-    path('signup/', views.signup, name='signup'),
-    
+    path("signup/", views.signup, name="signup"),
     # Profile
-    path('profile/', views.profile, name='profile'),
+    path("profile/", views.profile, name="profile"),
 ]
